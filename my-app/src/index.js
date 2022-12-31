bc7357dc00101fac05d7d1bff7539f77b86f5d85
Whoppa <joeynalvarado@gmail.com>
2022-12-31 09:43:48 -0600

    prop basics

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const title = 'Atomic Habits';
+const author = 'James Clear';
+const img =
+  'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg';
+const Booklist = () => {
+  return (
+    <section className="booklist">
+      <Book author={author} title={title} img={img} />
+      <Book author={author} title={title} img={img} />
+    </section>
+  );
+};
+
+const Book = props => {
+  console.log(props);
+  return (
+    <article className="book">
+      <img src={props.img} alt={props.title} />
+      <h2>{props.title}</h2>
+      <h4>{props.author}</h4>
+    </article>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(<Booklist />);
