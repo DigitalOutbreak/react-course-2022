a1b4a6cfe6513c193cfdcb5be352455ffb120422
Whoppa <joeynalvarado@gmail.com>
2022-12-31 12:41:45 -0600

    children props and simple lists

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,28 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
 import './index.css';
 
-const title = 'Atomic Habits';
-const author = 'James Clear';
-const img =
-  'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg';
+const books = [
+  {
+    author: 'Robert Greene',
+    title: 'The 48 Laws of Power',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL600_SR600,400_.jpg',
+  },
+  {
+    author: 'James Clear',
+    title: 'Atomic Habits',
+    img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+  },
+];
+
+const names = ['john', 'peter', 'susan'];
+const newNames = names.map((name) => {
+  return <h1>{name}</h1>;
+});
+
 const Booklist = () => {
-  return (
-    <section className="booklist">
-      <Book author={author} title={title} img={img} />
-      <Book author={author} title={title} img={img} />
-    </section>
-  );
+  return <section className="booklist">{newNames}</section>;
 };
 
-const Book = props => {
-  console.log(props);
+const Book = (props) => {
+  const { img, title, author, children } = props;
   return (
     <article className="book">
-      <img src={props.img} alt={props.title} />
-      <h2>{props.title}</h2>
-      <h4>{props.author}</h4>
+      <img src={img} alt={title} />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   );
 };
