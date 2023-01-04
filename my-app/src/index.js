da64505ae8952330c9acf21b8c7ad499f912545b
Whoppa <joeynalvarado@gmail.com>
2023-01-04 07:11:56 -0600

    35

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,31 +7,33 @@ const books = [
     author: 'Robert Greene',
     title: 'The 48 Laws of Power',
     img: 'https://images-na.ssl-images-amazon.com/images/I/71aG+xDKSYL._AC_UL600_SR600,400_.jpg',
+    id: 1,
   },
   {
     author: 'James Clear',
     title: 'Atomic Habits',
     img: 'https://images-na.ssl-images-amazon.com/images/I/81wgcld4wxL._AC_UL900_SR900,600_.jpg',
+    id: 2,
   },
 ];
 
-const names = ['john', 'peter', 'susan'];
-const newNames = names.map((name) => {
-  return <h1>{name}</h1>;
-});
-
 const Booklist = () => {
-  return <section className="booklist">{newNames}</section>;
+  return (
+    <section className="booklist">
+      {books.map((book) => {
+        return <Book {...book} key={book.id} />;
+      })}
+    </section>
+  );
 };
 
-const Book = (props) => {
-  const { img, title, author, children } = props;
+const Book = ({ img, title, author }) => {
+  // const { img, title, author } = props;
   return (
     <article className="book">
       <img src={img} alt={title} />
       <h2>{title}</h2>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
